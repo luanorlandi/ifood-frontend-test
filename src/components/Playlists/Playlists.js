df8cba3b19f5c29f61ee8720cc8746dcf9e123ee
Luan Orlandi <orlandi.luan@gmail.com>
2020-01-10 00:52:52 -0300

    Implement refresh

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -1,26 +1,51 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, {
+  useContext, useEffect, useState, useRef, useCallback,
+} from 'react';
 
 import SearchContext from '../../contexts/SearchContext';
 import getFeaturedPlaylists from '../../api/getFeaturedPlaylists';
 import PlaylistCard from './PlaylistCard';
+import constants from '../../constants';
 import './Playlists.scss';
+
+const { REFRESH_INTERVAL } = constants;
 
 const Playlists = () => {
   const { search } = useContext(SearchContext);
   const [playlists, setPlaylists] = useState(null);
+  const intervalId = useRef(null);
+
+  const restartInterval = useCallback(() => {
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+
+    intervalId.current = setInterval(() => {
+      getFeaturedPlaylists(search);
+    }, REFRESH_INTERVAL);
+  }, [search, intervalId]);
+
+  useEffect(() => {
+    restartInterval();
+
+    return () => {
+      clearInterval(intervalId.current);
+    };
+  }, [intervalId, restartInterval]);
 
   useEffect(() => {
     // TODO refresh a cada 30 segundos
     if (search !== null) {
       getFeaturedPlaylists(search)
         .then((newPlaylists) => setPlaylists(newPlaylists));
+      restartInterval();
     }
-  }, [search]);
+  }, [search, restartInterval]);
 
   return (
     <div className="playlists">
       {playlists && playlists.map((playlist) => (
-        <PlaylistCard playlist={playlist} />
+        <PlaylistCard key={playlist.id} playlist={playlist} />
       ))}
     </div>
   );
