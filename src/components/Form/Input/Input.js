d721c3143f4c806276079ff2df7d7c23bbe146af
Luan Orlandi <orlandi.luan@gmail.com>
2020-01-10 01:24:02 -0300

    Validate min and max in input field

--- src/components/Form/Input/Input.js
+++ src/components/Form/Input/Input.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import get from 'lodash/get';
 
-const Input = ({ formik, field, type }) => (
-  <div className="field">
-    <label htmlFor={field.id}>{field.name}</label>
-    <input
-      className="input"
-      id={field.id}
-      type={type} // TODO handle input type number (useful for mobile keyboard)
-      onChange={formik.handleChange}
-      value={formik.values[field.id]}
-    />
-  </div>
-);
+const Input = ({ formik, field, type }) => {
+  const min = get(field, 'validation.min');
+  const max = get(field, 'validation.max');
+
+  return (
+    <div className="field">
+      <label htmlFor={field.id}>{field.name}</label>
+      <input
+        className="input"
+        id={field.id}
+        type={type} // TODO handle input type number (useful for mobile keyboard)
+        onChange={formik.handleChange}
+        value={formik.values[field.id]}
+        min={min}
+        max={max}
+      />
+    </div>
+  );
+};
 
 Input.propTypes = {
   formik: PropTypes.object.isRequired,
