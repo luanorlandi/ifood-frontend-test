767a8ccd52659fbfae9a41c38c035493a67376f4
Luan Orlandi <orlandi.luan@gmail.com>
2020-01-13 14:25:25 -0300

    Fix https request

--- src/api/getFilters.js
+++ src/api/getFilters.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getFilters = async () => {
-  const response = await axios.get('http://www.mocky.io/v2/5a25fade2e0000213aa90776');
+  const response = await axios.get('https://www.mocky.io/v2/5a25fade2e0000213aa90776');
   return response.data.filters;
 };
 
